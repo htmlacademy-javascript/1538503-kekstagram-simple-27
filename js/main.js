318e558b59a66d6ce32a37384315de2aaa7039bc
Maksim Popov <wyoming228@gmail.com>
2022-10-07 10:16:32 +0300

    изменения согласно комментариям

--- js/main.js
+++ js/main.js
@@ -4,21 +4,19 @@ function getRandomIntInclusive (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  if (min < 0 || max < 0 || max <= min || typeof min !== 'number') {return NaN;
-  }
-
+  if (min < 0 || max < 0 || max <= min || typeof min !== 'number') {return NaN;}
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 getRandomIntInclusive(1,10);
 
-const lineLenght = function (lineName, maxLenght) {
-  if (maxLenght >= 20 && maxLenght < 141) {
+const lineLenght = function (line, maxLenght) {
+  if (line.length < maxLenght) {
     return true;
   }
 
   return false;
 };
 
-lineLenght('Имя', 130 );
+lineLenght('', 130 );
 
