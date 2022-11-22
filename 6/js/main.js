62f26548baec9e383f94d176eb98a5837186fb5b
Keks <keks@htmlacademy.ru>
2022-11-22 11:32:26 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/main.js
+++ 6/js/main.js
@@ -1,6 +1,9 @@
 import {displayRandomPhotos} from './random-thumbnail.js';
 import {uploadPhoto} from './user-form.js';
+import {formValidate} from './user-form.js';
 
 displayRandomPhotos();
 
 uploadPhoto();
+
+formValidate();
