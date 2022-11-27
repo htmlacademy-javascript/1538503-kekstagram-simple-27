78967d36ff3c85ea022e3b656d78a2dc3147408b
Maksim Popov <wyoming228@gmail.com>
2022-11-28 00:26:37 +0300

    Изменения

--- js/data.js
+++ js/data.js
@@ -7,7 +7,7 @@ const IDs = Array.from({length: ELEMENTS_COUNT}, (_, index) => index + 1);
 
 const ImageURLs = Array.from({length: ELEMENTS_COUNT}, (_, index) => `photos/${index + 1}.jpg`);
 
-const DESCRIPTIONS = ['Отличное фото', 'Мы с друзьями', 'Первые шаги Степы', 'Мамин юбилей', 'Вперед Спартак', 'Премьера фильма', 'Человек на Луне', 'Пикник в августе', 'Первая поездка на новом мотоцикле', 'Любовь и голуби'];
+const DESCRIPTIONS = ['Отличное фото', 'Мы с друзьями', 'Первые шаги Степы', 'Мамин юбилей', 'Вперед Спартак!', 'Премьера фильма', 'Человек на Луне', 'Пикник в августе', 'Первая поездка на новом мотоцикле', 'Любовь и голуби'];
 
 const getPictureDescribtion = () => ({
   id: getRandomArrayElement(IDs),
